1f93dac5a97804bf71bf05b6c319905962d07389
Sara Marques <a111182@alunos.uminho.pt>
2025-10-19 16:55:35 +0100

    deleted validation.h

--- trabalho-pratico/include/logic_validation.h
+++ trabalho-pratico/include/logic_validation.h
@@ -1,1 +1,8 @@
- 
+#include <stdbool.h> 
+
+// 5.3. Data Files Validation (.csv)
+
+
+// flight logic: In addition to syntactic errors, there may be certain logical inconsistencies.
+bool validate_flight_logic();
+
--- trabalho-pratico/include/sintatic_validation.h
+++ trabalho-pratico/include/sintatic_validation.h
@@ -3,8 +3,7 @@
 
 // 5.3. Data Files Validation (.csv)
 
-// Set of validations to be performed on received CSV format data. 
-// The validations to be performed are of two types: syntactic and logical.
+
 
 // syntax
 bool validate_date();
@@ -18,7 +17,4 @@ bool validate_reservation_id();
 bool validate_document_number();
 bool validate_airport_type();
 bool validate_csv_lists();
-
-// flight logic: In addition to syntactic errors, there may be certain logical inconsistencies.
-bool validate_flight_logic();
 
