12c401e25b3ce3e75fa960b76bfecfdc30be46dd
Sara Marques <a111182@alunos.uminho.pt>
2025-10-19 15:42:14 +0100

    added structs aircrafts and airports

--- trabalho-pratico/src/aircrafts.c
+++ trabalho-pratico/src/aircrafts.c
@@ -1,1 +1,10 @@
 #include <stdio.h>
+
+struct aircraft {
+    char id[15]; // identification (tail number)
+    char manufacturer[50]; // manufacturer of the aircraft
+    char model[50]; // model of the aircraft
+    char year[6]; // year of fabrication
+    int capacity[50]; // max capacity of the aircraft
+    int range[100]; // maximum range in km's
+};
--- trabalho-pratico/src/airports.c
+++ trabalho-pratico/src/airports.c
@@ -1,1 +1,13 @@
 #include <stdio.h>
+
+struct airport {
+    char code[4]; // 3 letters IATA
+    char name[100]; // name of the airport
+    char city[100]; // city where it's located
+    char country[50]; // country where it's located
+    double latitude[50]; // latitude of the airport
+    double longitude[50]; // longitude of the aiport
+    char icao[5]; // 4 letters ICAO
+    char type[30]; // type of airport
+};
+
