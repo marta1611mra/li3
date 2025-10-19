7e0a7b893cbd07cb160bdf9df56d6a743aa2baec
a111415 <a111415@alunos.uminho.pt>
2025-10-19 16:30:19 +0100

    struct passenger added

--- trabalho-pratico/src/passengers.c
+++ trabalho-pratico/src/passengers.c
@@ -1,1 +1,16 @@
 #include <stdio.h>
+
+struct passenger{
+
+    char document_id[10];
+    char first_name[30];
+    char last_name[30];
+    char dob[11];
+    char nationality[20];
+    char gender;
+    char email[50];
+    char phone[15];
+    char address[50];
+    char photo[100];
+
+};
