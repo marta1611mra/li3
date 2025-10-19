094636a2fdf037b7416ba103e4b436f9648cbad8
a111415 <a111415@alunos.uminho.pt>
2025-10-19 16:18:34 +0100

    structs flight_status and flight added

--- trabalho-pratico/src/flights.c
+++ trabalho-pratico/src/flights.c
@@ -1,1 +1,23 @@
 #include <stdio.h>
+
+typedef enum {
+    OnTime,
+    Delayded,
+    Cancelled
+} flight_status;
+
+struct flight{
+    
+    char flight_id[8];
+    char departure[19];
+    char actual_departure[19];
+    char arrival[19];
+    char actual_arrival[19];
+    char gate[3];
+    flight_status status;
+    char origin[4];
+    char destiny[4];
+    char aircraft_id[15];
+    char airline[40];
+    char tracking_url[100];
+};
