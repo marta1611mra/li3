2fd6dfe29120857a565bb2e6422b88d528a2e054
Marta Araújo <a110317@alunos.uminho.pt>
2025-11-02 16:33:02 +0000

    .

--- trabalho-pratico/include/managers/aircrafts_manager.h
+++ trabalho-pratico/include/managers/aircrafts_manager.h
@@ -2,6 +2,7 @@
 #define AIRCRAFTS_MANAGER_H
 #include "aircrafts.h"
 #include <glib.h>
+#include <stdbool.h>
 
 typedef struct aircrafts_manager *AircraftsManager;
 
@@ -10,5 +11,6 @@ void destroy_aircrafts_manager(AircraftsManager m);
 void aircrafts_manager_add(AircraftsManager m, Aircraft a);
 Aircraft aircrafts_manager_get(AircraftsManager m, const char *id);
 int aircrafts_manager_count(AircraftsManager m);
+bool aircrafts_manager_exists(AircraftsManager m, const char *id);
 
 #endif
