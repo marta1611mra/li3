b1e43a34e152423a7989d345be9f6b4214a6f092
Marta Araújo <a110317@alunos.uminho.pt>
2025-11-12 13:04:16 +0000

    correction

--- trabalho-pratico/src/queries/query1.c
+++ trabalho-pratico/src/queries/query1.c
@@ -11,6 +11,6 @@ void query1(AirportsManager am, const char *code, FILE *out) {
     Airport a = airports_manager_get(am, code);
 
     if(a) {
-        fprintf(out, "%s,%s,%s,%s,%s", get_airport_code(a), get_airport_name(a), get_airport_city(a), get_airport_country(a), get_airport_type(a));
+        fprintf(out, "%s,%s,%s,%s,%s\n", get_airport_code(a), get_airport_name(a), get_airport_city(a), get_airport_country(a), get_airport_type(a));
     }
 }
